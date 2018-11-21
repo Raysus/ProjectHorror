8f0e39a4f244fc925eed931c5876f2de8612a7a3
Raysus <dissapear.raysus@gmail.com>
2018-11-21 17:14:44 -0300

    agregado player, enemigos y algunas mecanicas

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate() {
+		InputManager();
+	}	
+
+	private void InputManager() 
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed;
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed;
+		}
+	}
+
+}
